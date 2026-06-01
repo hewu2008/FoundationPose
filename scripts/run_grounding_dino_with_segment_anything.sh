845397620f7eb8f97390c3147262fa704154226f
server <you@example.com>
2026-06-01 17:30:01 +0800

    1. add door opening limiter example

--- scripts/run_grounding_dino_with_segment_anything.sh
+++ scripts/run_grounding_dino_with_segment_anything.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python grounding_dino_with_segment_anything.py \
-    --image_url "http://images.cocodataset.org/val2017/000000039769.jpg" \
-    --labels "a cat.", "a remote control." \
+python scripts/grounding_dino_with_segment_anything.py \
+    --image_url "assets/door_opening_limiter.jpg" \
+    --labels "a door opening limiter." \
     --threshold 0.3 \
     --detector_id "/home/jszn/hewu/model_zoo/grounding-dino-tiny" \
     --segmenter_id "/home/jszn/hewu/model_zoo/sam-vit-base"
