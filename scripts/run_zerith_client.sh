791bb6a63c5bf38243fdb8adba09c23a70b1f7a0
server <you@example.com>
2026-06-01 12:32:02 +0800

    1. refactor client code

--- scripts/run_zerith_client.sh
+++ scripts/run_zerith_client.sh
@@ -2,7 +2,7 @@
 
 export PYTHONPATH=/home/jszn/hewu/alg-product/FoundationPose
 
-python zerith/zerith_foundation_pose_client.py \
+python zerith/zerith_client.py \
     --mesh_file "demo_data/mustard0/mesh/textured_simple.obj" \
     --video_dir "demo_data/mustard0" \
     --debug_dir "client_debug" \
