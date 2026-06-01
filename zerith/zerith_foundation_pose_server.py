f723c7f85faac298fdd091d777802b4f420d68f0
server <you@example.com>
2026-06-01 10:35:28 +0800

    1. add test code

--- zerith/zerith_foundation_pose_server.py
+++ zerith/zerith_foundation_pose_server.py
@@ -21,6 +21,8 @@ def parse_args():
     parser.add_argument('--zmq_port', type=int, default=5555, help='ZMQ server port')
     parser.add_argument('--detector_id', type=str, default="IDEA-Research/grounding-dino-tiny", help='Grounding DINO model ID')
     parser.add_argument('--segmenter_id', type=str, default="facebook/sam-vit-base", help='Segment Anything model ID')
+    parser.add_argument('--test_scene_dir', type=str, default=f'{code_dir}/demo_data/mustard0')
+    parser.add_argument('--debug_dir', type=str, default=f'{code_dir}/debug')
     return parser.parse_args()
 
 
@@ -53,7 +55,7 @@ class ZerithFoundationPoseServer:
             mesh=self.mesh, 
             scorer=self.scorer, 
             refiner=self.refiner, 
-            debug_dir=None, 
+            debug_dir=args.debug_dir, 
             debug=0, 
             glctx=self.glctx
         )
@@ -276,6 +278,30 @@ class ZerithFoundationPoseServer:
                 'message': str(e)
             }
     
+    def execute_full_pipeline(self):
+        reader = YcbineoatReader(video_dir=args.test_scene_dir, shorter_side=None, zfar=np.inf)
+        for i in range(len(reader.color_files)):
+            logging.info(f'i: {i}')
+            color = reader.get_color(i)
+            depth = reader.get_depth(i)
+            if i == 0:
+                mask = reader.get_mask(0).astype(bool)
+                pose = self.register(K=reader.K, rgb=color, depth=depth, ob_mask=mask)
+            else:
+                pose = self.track(K=reader.K, rgb=color, depth=depth)
+        
+            os.makedirs(f'{args.debug_dir}/ob_in_cam', exist_ok=True)
+            np.savetxt(f'{args.debug_dir}/ob_in_cam/{reader.id_strs[i]}.txt', pose.reshape(4,4))
+
+            center_pose = pose@np.linalg.inv(self.to_origin)
+            vis = draw_posed_3d_box(reader.K, img=color, ob_in_cam=center_pose, bbox=self.bbox)
+            vis = draw_xyz_axis(color, ob_in_cam=center_pose, scale=0.1, K=reader.K, thickness=3, transparency=0, is_input_rgb=True)
+            cv2.imshow('1', vis[...,::-1])
+            cv2.waitKey(1)
+
+            os.makedirs(f'{args.debug_dir}/track_vis', exist_ok=True)
+            imageio.imwrite(f'{args.debug_dir}/track_vis/{reader.id_strs[i]}.png', vis)
+
     def start(self, port=5555):
         """Start ZMQ server with register and track interfaces"""
         context = zmq.Context()
