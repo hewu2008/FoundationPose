46890ae0ec02ced3ae4947ac763c129b192b938f
server <you@example.com>
2026-06-03 09:49:19 +0800

    1. 增加水壶的描述

--- scripts/run_grounding_dino_with_segment_anything.sh
+++ scripts/run_grounding_dino_with_segment_anything.sh
@@ -2,7 +2,7 @@
 
 python scripts/grounding_dino_with_segment_anything.py \
     --image_url "assets/zerith_rgb.png" \
-    --labels "a black part" \
-    --threshold 0.3 \
+    --labels "white brake fluid reservoir" \
+    --threshold 0.4 \
     --detector_id "/home/jszn/hewu/model_zoo/grounding-dino-tiny" \
     --segmenter_id "/home/jszn/hewu/model_zoo/sam-vit-base"
