4a247beacc6a72de029e4a87404a7304a132a1e1
server <you@example.com>
2026-06-15 15:30:52 +0800

    fix(scripts): 更新启动脚本的模型和场景路径
    将启动脚本中的网格文件、调试目录路径替换为实际的瓶子模型和对应调试目录，修正启动配置路径

--- scripts/run_zerith_server.sh
+++ scripts/run_zerith_server.sh
@@ -5,7 +5,7 @@ export PYTHONPATH=/home/jszn/hewu/alg-product/FoundationPose
 python zerith/zerith_server_main.py \
     --detector_id "/home/jszn/hewu/model_zoo/grounding-dino-tiny" \
     --segmenter_id "/home/jszn/hewu/model_zoo/sam-vit-base" \
-    --mesh_file "demo_data/mustard0/mesh/textured_simple.obj" \
+    --mesh_file "assets/DPPUB-204001196-AAX_01_01.obj" \
     --test_scene_dir "demo_data/mustard0" \
-    --debug_dir "debug_mard0" \
+    --debug_dir "debug_bottle" \
     --zmq_port 5555
