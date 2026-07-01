7a1d4e723d426abe5e1b4f7859161fa9ef020949
server <you@example.com>
2026-07-01 09:41:26 +0800

    chore(scripts): 更新grounding dino推理参数
    调整了目标标签和置信度阈值，适配新的检测场景

--- scripts/run_grounding_dino_with_segment_anything.sh
+++ scripts/run_grounding_dino_with_segment_anything.sh
@@ -2,7 +2,7 @@
 
 python scripts/grounding_dino_with_segment_anything.py \
     --image_url "assets/zerith_rgb.png" \
-    --labels "translucent white brake fluid reservoir" \
-    --threshold 0.34 \
+    --labels "small white plastic tank with black cap" \
+    --threshold 0.3 \
     --detector_id "/home/jszn/hewu/model_zoo/grounding-dino-tiny" \
     --segmenter_id "/home/jszn/hewu/model_zoo/sam-vit-base"
