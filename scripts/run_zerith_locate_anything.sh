b93c9e331d2634af1afcf6a6466c9d64476266dd
server <you@example.com>
2026-07-01 16:45:54 +0800

    refactor(scripts): rename locate_anything相关文件并更新启动脚本
    将原有的locate_anything.py重命名为zerith_locate_anything.py，同时新增对应的启动脚本run_zerith_locate_anything.sh，删除旧的run_locate_anything.sh脚本

--- scripts/run_zerith_locate_anything.sh
+++ scripts/run_zerith_locate_anything.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python zerith/locate_anything.py \
+python zerith/zerith_locate_anything.py \
     --model_path "/home/jszn/hewu/model_zoo/LocateAnything-3B" \
     --image "/home/jszn/hewu/dataset/locate_anything/28.png"
