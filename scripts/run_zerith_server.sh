15a7a997c4321af09f852b56fc7c34dabed606ef
server <you@example.com>
2026-07-01 10:01:31 +0800

    chore(scripts): 更新启动脚本的默认参数
    移除测试场景目录和旧调试目录配置，修改调试目录为runtime/debug_zerith_server

--- scripts/run_zerith_server.sh
+++ scripts/run_zerith_server.sh
@@ -6,6 +6,5 @@ python zerith/zerith_server_main.py \
     --detector_id "/home/jszn/hewu/model_zoo/grounding-dino-tiny" \
     --segmenter_id "/home/jszn/hewu/model_zoo/sam-vit-base" \
     --mesh_file "assets/DPPUB-204001196-AAX_01_01.obj" \
-    --test_scene_dir "demo_data/mustard0" \
-    --debug_dir "debug_bottle" \
+    --debug_dir "runtime/debug_zerith_server" \
     --zmq_port 5555
