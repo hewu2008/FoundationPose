66f2340d2d78eea3d5d93a40ac4743f05d13cc00
server <you@example.com>
2026-07-01 09:58:38 +0800

    chore: 更新gitignore和运行脚本配置
    1. 新增runtime目录到git忽略列表
    2. 修改调试输出目录为runtime下的子目录
    3. 更新模型识别的标签文本

--- scripts/run_zerith_client.sh
+++ scripts/run_zerith_client.sh
@@ -5,5 +5,5 @@ export PYTHONPATH=/home/jszn/hewu/alg-product/FoundationPose
 python zerith/zerith_client.py \
     --mesh_file "assets/DPPUB-204001196-AAX_01_01.obj" \
     --video_dir "demo_data/mustard0" \
-    --debug_dir "debug_bottle" \
-    --labels "translucent white brake fluid reservoir"
+    --debug_dir "runtime/debug_zerith_client" \
+    --labels "small white plastic tank with black cap"
